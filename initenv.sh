598da83129400df501a6b76190a6172ebae90429
Christian Drexler <christian@konsolenknecht.de>
2023-04-02 13:44:56 +0200

    added psqlrc to init script

--- initenv.sh
+++ initenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 githubbase="https://github.com/xunzi/dotfiles/raw/master"
 
-	for f in .tmux.conf .zshrc .vimrc .emacs.d ; do
+	for f in .tmux.conf .zshrc .vimrc .emacs.d .psqlrc ; do
 		echo "Copying ${f}"
 		cp -r ${f} $HOME
 	done
